9cf15c4e165915e5301f2ac94cd071b1429b7dd8
Thomas Kaczmarek <mulenatic@gmail.com>
2016-05-07 17:43:41 +0200

    Starting point for developing the auction application

---
+++ chapter02/auction/app/app.ts
@@ -0,0 +1,19 @@
+import {bootstrap} from 'angular2/platform/browser';
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'app',
+    template: `<h1>Hello {{name}}!</h1>`})
+/**
+ * AppComponent
+ */
+class AppComponent {
+    
+    name: string;
+    
+    constructor() {
+        this.name = 'Angular 2';
+    }
+}
+
+bootstrap(AppComponent);
