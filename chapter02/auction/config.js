76e15838e1a61f3b429b614304f34cf05df75075
Thomas Kaczmarek <mulenatic@gmail.com>
2016-05-07 17:50:09 +0200

    Added a config file for the application.

---
+++ chapter02/auction/config.js
@@ -0,0 +1,14 @@
+System.config({
+    baseURL: "/",
+    transpiler: "typescript",
+    typescriptOptions: {
+        "emitDecoratorMetadata": true
+    },
+    
+    packages: {
+        "app": {
+            "defaultExtension": "ts",
+            "main": "main.ts"
+        }
+    }
+});
