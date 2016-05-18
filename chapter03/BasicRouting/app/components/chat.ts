f0508397ee989783cf37dd8a543ee131f7a88a42
Thomas Kaczmarek <mulenatic@gmail.com>
2016-05-18 21:19:24 +0200

    Included an auxilary route for "customer service chat"

---
+++ chapter03/BasicRouting/app/components/chat.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'chat',
+    template: `<textarea placeholder="Chat with customer service" class="chat"></textarea>`,
+    styles: [`.chat {background: #eee; height: 80px; width: 30%; font-size: 30px; float:leaf: display:block; box-sizing: border-box;}`]})
+export class ChatComponent{}
--- chapter03/basicRouting/app/main.ts
+++ chapter03/basicRouting/app/main.ts
@@ -2,6 +2,7 @@ import {bootstrap} from 'angular2/platform/browser';
 import {Component, provide} from 'angular2/core';
 import {HomeComponent} from './components/home';
 import {ProductDetailComponent} from './components/product';
+import {ChatComponent} from './components/chat';
 
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 import {LocationStrategy, HashLocationStrategy} from 'angular2/router';
@@ -9,15 +10,23 @@ import {LocationStrategy, HashLocationStrategy} from 'angular2/router';
 
 @Component({
     selector: "app",
-    template: `<a [routerLink]="['/Home']">Home</a>
-               <a [routerLink]="['/ProductDetail', {id: 1234}, 'ProductDescription']">Product Details</a>
+    template: `<div>
+                <a [routerLink]="['/Home']">Home</a>
+                <a [routerLink]="['/ProductDetail', {id: 1234}, 'ProductDescription']">Product Details</a>
+                <a [routerLink]="['./Home', ['Chat']]">Chat</a>
+               </div>
                
-               <router-outlet></router-outlet>`,
+               <div>               
+                <router-outlet></router-outlet>
+                <router-outlet name="chat"></router-outlet>
+               </div>`,
     directives: [ROUTER_DIRECTIVES]
 })
 @RouteConfig([
     {path: '/', component: HomeComponent, as: 'Home'},
-    {path: '/product/:id/...', component: ProductDetailComponent, as: 'ProductDetail', data: {isProd: true}}])
+    {path: '/product/:id/...', component: ProductDetailComponent, as: 'ProductDetail', data: {isProd: true}},
+    {aux: '/chat', component: ChatComponent, as: 'Chat'}    
+])
 class AppComponent{}
 
 bootstrap(AppComponent, [ROUTER_PROVIDERS, provide(LocationStrategy, {useClass: HashLocationStrategy})]);
