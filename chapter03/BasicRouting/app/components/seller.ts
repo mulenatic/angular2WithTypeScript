f79e3bbdac662ca8a33e670627664a562e8de619
Thomas Kaczmarek <mulenatic@gmail.com>
2016-05-18 20:50:31 +0200

    Fixed a typo.

--- chapter03/BasicRouting/app/components/seller.ts
+++ chapter03/BasicRouting/app/components/seller.ts
@@ -3,5 +3,5 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'seller',
     template: '<p>The seller of this product is Mary Lou (98% positive feedback)',
-    styles: [':host {background: yelloy}']})
+    styles: [':host {background: yellow}']})
 export class SellerInfoComponent{}
