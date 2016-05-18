1ac72c319fa49585bb25cebac28d4de38ddc7285
Thomas Kaczmarek <mulenatic@gmail.com>
2016-05-18 21:35:51 +0200

    Added lazy loading of a page.

--- chapter03/basicRouting/app/main.ts
+++ chapter03/basicRouting/app/main.ts
@@ -11,13 +11,20 @@ import {LocationStrategy, HashLocationStrategy} from 'angular2/router';
     selector: "app",
     template: `<a [routerLink]="['/Home']">Home</a>
                <a [routerLink]="['/ProductDetail', {id: 1234}, 'ProductDescription']">Product Details</a>
+               <a [routerLink]="['/Luxury']">Luxury Items</a>
                
                <router-outlet></router-outlet>`,
     directives: [ROUTER_DIRECTIVES]
 })
 @RouteConfig([
     {path: '/', component: HomeComponent, as: 'Home'},
-    {path: '/product/:id/...', component: ProductDetailComponent, as: 'ProductDetail', data: {isProd: true}}])
+    {path: '/product/:id/...', component: ProductDetailComponent, as: 'ProductDetail', data: {isProd: true}},
+    {path: '/luxury',
+    loader: () => System.import('app/components/luxury')
+        .then(libModule => libModule.LuxuryComponent)
+        .catch(err => console.error("Can't load luxury items: ${err}")), 
+    as: 'Luxury'}    
+])
 class AppComponent{}
 
 bootstrap(AppComponent, [ROUTER_PROVIDERS, provide(LocationStrategy, {useClass: HashLocationStrategy})]);
