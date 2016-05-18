7a0736a23ff0599a9bc49c6f1abab61f64a159a1
Thomas Kaczmarek <mulenatic@gmail.com>
2016-05-18 23:26:07 +0200

    Bugfixed the whole thing, now it's working. Almost each file had a small problem. What's import is that the html has to be correct too!!!

--- chapter03/auction/app/components/application/application.ts
+++ chapter03/auction/app/components/application/application.ts
@@ -1,11 +1,10 @@
 import {Component} from 'angular2/core';
 import {Route, RouteConfig, RouterOutlet} from 'angular2/router';
-import HomeComponent from 'app/components/home/home';
-import FooterComponent from 'app/components/footer/footer';
-import NavbarComponent from 'app/components/navbar/navbar';
-import SearchComponent from 'app/components/search/search';
+import HomeComponent from '../home/home';
+import FooterComponent from '../footer/footer';
+import NavbarComponent from '../navbar/navbar';
+import SearchComponent from '../search/search';
 import {ProductDetailComponent} from '../product-detail/product-detail';
-import {Product, ProductService} from 'app/services/product-service';
 
 @Component({
     selector: 'auction-application',
@@ -22,11 +21,4 @@ import {Product, ProductService} from 'app/services/product-service';
     {path: '/', component: HomeComponent, as: 'Home'},
     {path: '/products/:prodTitle', component: ProductDetailComponent, as: 'ProductDetail'}    
 ])
-export default class ApplicationComponent {
-    
-    products: Array<Product> = [];
-    
-    constructor(private productService: ProductService) {
-        this.products = this.productService.getProducts();
-    }
-}
+export default class ApplicationComponent {}
--- chapter03/auction/app/components/home/home.ts
+++ chapter03/auction/app/components/home/home.ts
@@ -1,31 +1,25 @@
 import {Component} from 'angular2/core';
-import {Product, ProductService} from 'app/services/product-service';
+import {Product, ProductService} from '../../services/product-service';
 import CarouselComponent from '../carousel/carousel';
 import ProductItemComponent from '../product-item/product-item';
 
 @Component({
     selector: 'auction-home-page',
     providers: [ProductService],
-    template: `
-    <div class="row carousel-holder>
-        <div class="col-md-12">
-            <auction-carousel></auction-carousel>
-        </div>
-    </div>
-    <div class="row">
-        <div *ngFor="#product of products" class="col-sm-4 col-lg-4 col-md-4">
-            <auction-product-item [product]="product"></auction-product-item>
-        </div>
-    </div>`,
-    styleUrls: ['/home.css'],
+    templateUrl: 'app/components/home/home.html'
+    styleUrls: ['app/components/home/home.css'],
     directives: [
         CarouselComponent, ProductItemComponent
     ]
 })
 export default class HomeComponent{
+    
     products: Product[] = [];
     
     constructor(private productService: ProductService) {
         this.products = this.productService.getProducts();
     }
+    
 }
+
+    
