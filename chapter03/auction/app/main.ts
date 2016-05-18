29563341fdfc35422b990d4b7fe08d0969b0ba34
Thomas Kaczmarek <mulenatic@gmail.com>
2016-05-18 22:22:55 +0200

    Updated the main.ts file.

--- chapter03/auction/app/main.ts
+++ chapter03/auction/app/main.ts
@@ -1,4 +1,6 @@
 import {bootstrap} from 'angular2/platform/browser';
+import {provide} from 'angular2/core';
 import ApplicationComponent from 'app/components/application/application';
+import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
 
-bootstrap(ApplicationComponent);
+bootstrap(ApplicationComponent, [ROUTER_PROVIDERS, provide(LocationStrategy, {useClass: HashLocationStrategy})]);
