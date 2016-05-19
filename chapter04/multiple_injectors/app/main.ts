6b6f5419fec176dad695a7f9b7d2f7d186bb65e5
Thomas Kaczmarek <mulenatic@gmail.com>
2016-05-19 22:41:25 +0200

    Adding new project for exploring changing the Implementation of services/components via injection. In this version, the root component injects the two components, where the second implements the first (weird, but in typescript this is possible).

---
+++ chapter04/multiple_injectors/app/main.ts
@@ -0,0 +1,57 @@
+import {bootstrap} from 'angular2/platform/browser';
+import {Component, provide} from 'angular2/core';
+
+class Product {
+    constructor(public title: string) {}
+}
+
+class ProductService {
+    getProduct() : Product {
+        // Code making an HTTP request would go here
+        return new Product('iPhone 7');
+    }
+}
+
+class MockProductService implements ProductService {
+    getProduct() : Product {
+        return new Product("Samsung 7");
+    }
+}
+
+@Component({
+    selector: 'product1',
+    template: '{{product.title}}'
+})
+class Product1Component {
+    product : Product;
+    
+    constructor(productService : ProductService) {
+        this.product = productService.getProduct();
+    }
+}
+
+@Component({
+    selector: 'product2',
+    providers: [provide(ProductService, {useClass: MockProductService})],
+    template: '{{product.title}}'
+})
+class Product2Component {
+    product: Product;
+    
+    constructor( productService: ProductService) {
+        this.product = productService.getProduct();        
+    }
+}
+
+@Component({
+    selector: 'app',
+    directives: [Product1Component, Product2Component],
+    template: `<h2>A root component hosts two products<br> provided by different services</h2>
+    <product1></product1><br>
+    <product2></product2>`
+})
+class RootComponent{}
+
+const DEFAULT_SERVICE_PROVIDERS = [ProductService];
+
+bootstrap(RootComponent, [DEFAULT_SERVICE_PROVIDERS]);
