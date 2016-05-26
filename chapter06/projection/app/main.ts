7b63f402eed8f717d01b3845c1aeb08d48d2df40
Thomas Kaczmarek <mulenatic@gmail.com>
2016-05-26 18:38:55 +0200

    Completed the example for projection in angular, where a parent component can project html into the child component. This version is not working in Safari, but in Google Chrome.

--- chapter06/projection/app/main.ts
+++ chapter06/projection/app/main.ts
@@ -1,18 +1,34 @@
 import {bootstrap} from 'angular2/platform/browser';
-import {Component} from 'angular2/core';
+import {Component, ViewEncapsulation} from 'angular2/core';
 
 @Component({
-    selector: "app",
-    template: `<h1>Hello {{name}}</h1>`
+    selector: 'child',
+    styles: ['.wrapper {background: lightgreen;}'],
+    template: `
+<div class="wrapper">
+  <h2>Child</h2>
+    <div>This &lt;div&gt; is defined in the child's template</div>
+    <ng-content></ng-content>
+</div>`,
+    encapsulation: ViewEncapsulation.Native
 })
-class AppComponent{
-    
-    name: string;
-    
-    constructor() {
-        this.name = "Angular 2";
-    }
-    
-}
+class ChildComponent { }
+
+@Component({
+    selector: 'app',
+    styles: ['.wrapper {background: cyan;}'],
+    directives: [ChildComponent],
+    template: `
+<div class="wrapper">
+  <h2>Parent</h2>
+    <div>This &lt;div&gt; is defined in the parent's template</div>
+  <child>
+    <div>Parent projects this &lt;div&gt; onto the child</div>
+  </child>
+</div>`,
+
+    encapsulation: ViewEncapsulation.Native
+})
+class AppComponent { }
 
 bootstrap(AppComponent);
