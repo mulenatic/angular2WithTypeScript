d2531f430262a8ad4a01b4bf06c80c90b4884c95
Thomas Kaczmarek <mulenatic@gmail.com>
2016-05-27 20:19:33 +0200

    Improvement of the auction application. Now reviews to articles can be given, this uses some of the techniques shown in this chapter as inter component communication and so on.

--- chapter06/auction/app/components/product-detail/product-detail.ts
+++ chapter06/auction/app/components/product-detail/product-detail.ts
@@ -5,18 +5,48 @@ import StarsComponent from '../stars/stars';
 
 @Component({
     selector: 'auction-product-page',
+    styles: ['auction-stars.large {font-size: 24px;}'],
     templateUrl: 'app/components/product-detail/product-detail.html',
     directives: [RouterLink, StarsComponent]
 })
-export class ProductDetailComponent{
+export class ProductDetailComponent {
     product: Product;
     reviews: Review[];
-    
+
+    newComment: string;
+    newRating: number;
+    isReviewHidden: boolean = true;
+
     constructor(params: RouteParams, productService: ProductService) {
-        
-        let prodId: number = parseInt(params.get('productId'));
-        this.product = productService.getProductById(prodId);
-        
+
+        this.product = productService
+            .getProducts()
+            .find(p => p.id === parseInt(params.get('productId')));
+
         this.reviews = productService.getReviewsForProduct(this.product.id);
     }
+
+    addReview() {
+        let review = new Review(0, this.product.id, new Date(), "Anonymous", this.newRating, this.newComment);
+        this.reviews = [...this.reviews, review];
+        this.product.rating = this.averageRating(this.reviews);
+
+        this.resetForm();
+    }
+
+
+    averageRating(reviews: Review[]) {
+        let sum = reviews.reduce((average, review) => average + review.rating, 0);
+
+        return sum / reviews.length;
+    }
+
+    resetForm() {
+        this.newRating = 0;
+        this.newComment = null;
+        this.isReviewHidden = true;
+    }
+
+
+
 }
--- chapter06/auction/app/components/stars/stars.ts
+++ chapter06/auction/app/components/stars/stars.ts
@@ -1,21 +1,37 @@
-import {Component, Input, OnInit} from 'angular2/core';
+import {Component, EventEmitter, Input, Output} from 'angular2/core';
+
+
 
 @Component({
-    templateUrl: 'app/components/stars/stars.html',
+    selector: 'auction-stars',
     styles: [`.starrating {color: #d17581; }`],
-    selector: 'auction-stars'
+    templateUrl: 'app/components/stars/stars.html'
 })
-export default class StarsComponent implements OnInit {
-    
-    @Input() count: number = 5;
-    
-    @Input() rating: number = 0;
-    
-    stars: boolean[] = [];
-    
-    ngOnInit() {
-        for (let i = 1; i<= this.count; i++ ) {
-            this.stars.push(i > this.rating);
+export default class StarsComponent {
+
+    private _rating: number;
+    private stars: boolean[];
+
+    private maxStars: number = 5;
+
+    @Input() readonly: boolean = true;
+
+    @Input() get rating(): number {
+        return this._rating;
+    }
+
+    set rating(value: number) {
+        this._rating = value || 0;
+        this.stars = Array(this.maxStars).fill(true, 0, this.rating);
+    }
+
+    @Output() ratingChange: EventEmitter<number> = new EventEmitter();
+
+    fillStarsWithColor(index) {
+
+        if (!this.readonly) {
+            this.rating = index + 1;
+            this.ratingChange.emit(this.rating);
         }
     }
 
