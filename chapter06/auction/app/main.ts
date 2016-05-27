d50574bac758c118940058847feb4b51a5ed4f58
Thomas Kaczmarek <mulenatic@gmail.com>
2016-05-27 18:46:06 +0200

    Next step for the auction application, copied the version from chapter 5.

---
+++ chapter06/auction/app/main.ts
@@ -0,0 +1,7 @@
+import {bootstrap} from 'angular2/platform/browser';
+import {provide} from 'angular2/core';
+import ApplicationComponent from './components/application/application';
+import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
+import {ProductService} from './services/product-service';
+
+bootstrap(ApplicationComponent, [ROUTER_PROVIDERS, provide(LocationStrategy, {useClass: HashLocationStrategy}), ProductService]);
