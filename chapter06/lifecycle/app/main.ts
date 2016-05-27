1cec2b5b22de8b7b703e6bf14d928dcef1a27464
Thomas Kaczmarek <mulenatic@gmail.com>
2016-05-27 11:07:25 +0200

    This example shows how to use the ngOnChanges lifecycle hook, which is called each time, the value of an Input property is changed. However, it only reacts, if the underlying object or primitive is changed, so editing the property of an object won't be detected.

--- chapter06/lifecycle/app/main.ts
+++ chapter06/lifecycle/app/main.ts
@@ -1,18 +1,46 @@
 import {bootstrap} from 'angular2/platform/browser';
-import {Component} from 'angular2/core';
+import {Component, Input, OnChanges, SimpleChange} from 'angular2/core';
+
+@Component({
+    selector: 'child',
+    styles: ['.child {background: lightgreen;}'],
+    template: `
+<div class="child">
+  <h2>Child</h2>
+  <div>Greeting: {{greeting}}</div>
+  <div>User name: {{user.name}}</div>
+  <div>Message: <input [(ngModel)]="message"></div>
+</div>`})
+class ChildComponent implements OnChanges {
+
+    @Input() greeting: string;
+    @Input() user: { name: string };
+
+    message: string = 'Initial message';
+
+    ngOnChanges(changes: { [propName: string]: SimpleChange }) {
+        console.log(JSON.stringify(changes, null, 2));
+    }
+}
+
 
 @Component({
     selector: "app",
-    template: `<h1>Hello {{name}}</h1>`
+    directives: [ChildComponent],
+    styles: ['.parent {background: lightblue;}'],
+    template: `
+<div class="parent">
+  <h2>Parent</h2>
+  <div>Greeting: <input type="text" [value]="greeting" (change)="greeting = $event.target.value"></div>
+  <div>User name: <input type="text" [value]="user.name" (change)="user.name = $event.target.value"></div>
+  <child [greeting]="greeting" [user]="user"></child>
+</div>`
 })
-class AppComponent{
-    
-    name: string;
-    
-    constructor() {
-        this.name = "Angular 2";
-    }
-    
+class AppComponent {
+
+    greeting: string = 'Hello';
+    user: { name: string } = { name: 'John' };
+
 }
 
 bootstrap(AppComponent);
