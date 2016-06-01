3e64f4f52af31e15421339f57e0f7fa78c881dd2
Thomas Kaczmarek <mulenatic@gmail.com>
2016-06-01 21:31:05 +0200

    Build the example using forms in Angular2 in the auction application. In the example a formbuilder is used to set up the form. However, this approach still lacks from a Angular bug which prevents this to function directly. A workaround is to wrap the formcomponets in the template into a <div *ngIf="true"></div. block.

--- chapter07/auction/app/components/search/search.ts
+++ chapter07/auction/app/components/search/search.ts
@@ -1,7 +1,40 @@
-import {Component} from 'angular2/core';
+import {Component} from "angular2/core";
+import {Control, ControlGroup, Validators, FormBuilder, FORM_PROVIDERS, FORM_DIRECTIVES} from "angular2/common";
+import {ProductService} from "../../services/product-service";
 
 @Component({
-    selector: 'auction-search',
-    templateUrl: 'app/components/search/search.html'
+    selector: "auction-search",
+    templateUrl: "app/components/search/search.html",
+    directives: [FORM_DIRECTIVES],
+    providers: [ProductService, FORM_PROVIDERS]
 })
-export default class SearchComponent {}
+export default class SearchComponent {
+
+    formModel: ControlGroup;
+    categories: string[];
+
+    constructor(private productService: ProductService) {
+        this.categories = this.productService.getAllCategories();
+        const fb = new FormBuilder();
+        this.formModel = fb.group({
+            title: [null, Validators.minLength(3)],
+            price: [null, positiveNumberValidator],
+            category: [-1]
+        });
+    }
+
+
+    onSearch() {
+        if (this.formModel.valid) {
+            console.log(this.formModel.value);
+        }
+    }
+}
+
+function positiveNumberValidator(control: Control): any {
+    if (!control.value) return null;
+    const price = parseInt(control.value);
+
+    return price === null || typeof price === 'number' && price > 0 ? null : { positivenumber: true };
+
+}
