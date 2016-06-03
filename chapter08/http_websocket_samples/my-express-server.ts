33ce683c5f25aa87a3357bdd8804715aa5580322
Thomas Kaczmarek <mulenatic@gmail.com>
2016-06-03 14:11:53 +0200

    Building the basic API.
    The server now delivers a basic API where /prodcuts will return all
    known products and /products/:id the product with the given id.

--- chapter08/http_websocket_samples/my-express-server.ts
+++ chapter08/http_websocket_samples/my-express-server.ts
@@ -2,14 +2,38 @@ import * as express from "express";
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello from Express'));
+class Product {
+    constructor(
+        public id: number,
+        public title: string,
+        public price: number
+    ) { }
+}
 
-app.get('/products', (req, res) => res.send('Got a request for products'));
+const products = [
+    new Product(0, "First product", 24.99),
+    new Product(1, "Second product", 64.99),
+    new Product(2, "Third product", 74.99)
+];
 
-app.get('/reviews', (req, res) => res.send('Got a request for reviews'));
+function getProducts(): Product[] {
+    return products;
+}
+
+app.get("/", (req, res) => res.send("The URL for products is http://localhost:8000/products"));
+
+app.get("/products", (req, res) => res.json(getProducts()));
+
+function getProductsById(productId: number): Product {
+    return products.find(p => p.id === productId);
+}
+
+app.get("/products/:id", (req, res) => {
+    res.json(getProductsById(parseInt(req.params.id)));
+});
 
 const server = app.listen(8000, "localhost", () => {
     const {address, port} = server.address();
 
-    console.log('Listening on http://localhost:' + port);
-})
+    console.log("Listening on http://localhost:" + port);
+});
