9805cd3d22e469c985fec38515fef61db92673d8
Thomas Kaczmarek <mulenatic@gmail.com>
2016-06-03 18:34:41 +0200

    Using async pipe in template to unwrap products.
    This version of the example app uses the async pipe to unwrap the
    products out of the Observable.

--- chapter08/http_websocket_samples/client/app/main.ts
+++ chapter08/http_websocket_samples/client/app/main.ts
@@ -7,7 +7,7 @@ import "rxjs/add/operator/map";
 @Component({
     selector: "http-client",
     template: `<h1>All Products</h1>
-  <li *ngFor="#product of products">
+  <li *ngFor="#product of products | async ">
     {{product.title}} - {{product.price | currency}}
   </li>
 <ul>
@@ -16,30 +16,13 @@ import "rxjs/add/operator/map";
 class AppComponent {
 
     // the Product class is not available for angular in this example, in a real app, this should be better.
-    products: Array<any> = [];
+    products: Observable<Array<any>>;
 
-    theDataSource: Observable<any>;
 
     constructor(private http: Http) {
 
-        this.theDataSource = this.http.get("/products").map(res => res.json());
+        this.products = http.get("/products").map(res => res.json());
 
-    }
-
-    ngOnInit() {
-
-        // Get the data from the server
-        this.theDataSource.subscribe(
-            data => {
-                if (Array.isArray(data)) {
-                    this.products = data;
-                } else {
-                    this.products.push(data);
-                }
-            },
-            err => console.log("Can't get products. Error code %s, URL: %s", err.status, err.url),
-            () => console.log('Product(s) are retrieved')
-        );
     }
 
 
