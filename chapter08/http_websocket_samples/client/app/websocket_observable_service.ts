581906cbf95fed453ba1b32f18c0acd06b16ee89
Thomas Kaczmarek <mulenatic@gmail.com>
2016-06-05 14:51:12 +0200

    Added a "Observable WebSocket" Service.
    Added a new Service which wraps a WebSocket within an Observable.

---
+++ chapter08/http_websocket_samples/client/app/websocket_observable_service.ts
@@ -0,0 +1,23 @@
+import {Observable, Subscriber} from "rxjs/Rx";
+
+export class WebSocketService {
+
+    ws: WebSocket;
+
+    createObservableSocket(url: string): Observable<any> {
+
+        this.ws = new WebSocket(url);
+
+        return new Observable(
+            observer => {
+
+                this.ws.onmessage = (event) => observer.next(event.data);
+
+                this.ws.onerror = (event) => observer.error(event);
+
+                this.ws.onclose = (event) => observer.complete();
+            }
+        );
+
+    }
+}
